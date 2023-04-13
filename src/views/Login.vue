5c149fdd6b3e9f1a92d33f90c02166fdf6c74219
fycoder <gemma.mobu@icloud.com>
2023-04-13 14:42:27 +0800

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<!-- name: '' -->
+<template>
+   <div></div>
+</template>
+
+<script lang="ts">
+export default {
+   data () {
+      return {
+
+      };
+   },
+   components: {
+
+   },
+   computed: {
+
+   },
+   methods: {
+
+   }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
